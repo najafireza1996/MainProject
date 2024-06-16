7bfbf9fadff2678915169089e60b5964026f8799
reza najafi <najafireza1996@gmail.com>
2024-06-17 03:08:15 +0330

    Feat(Core):Added sms services.

---
+++ MainProject/Features/Core/Application/IServices/ISmsService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Features.User.Domain.Entities;
+
+namespace MainProject.Features.Core.Application.IServices
+{
+    public interface ISmsService
+    {
+        public Task SendAsync(ApplicationUser user);
+    }
+}
+
---
+++ MainProject/Features/Core/Application/Services/SmsService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using IPE.SmsIrClient.Models.Requests;
+using IPE.SmsIrClient.Models.Results;
+using IPE.SmsIrClient;
+using MainProject.Features.Core.Application.IServices;
+using Features.User.Domain.Entities;
+
+namespace MainProject.Features.Core.Application.Services
+{
+    public class SmsService : ISmsService
+    {
+        public async Task SendAsync(ApplicationUser user)
+        {
+            var smsIr = new SmsIr("temp");
+
+
+            var templateId = 791511;
+            VerifySendParameter[] verifySendParameters = {
+            new VerifySendParameter("NAME", user.FirstName!=null && user.FirstName != string.Empty?user.FirstName:"کاربر گرامی"),
+            // new VerifySendParameter("CODE", user.OTP_Code.ToString()),
+            new VerifySendParameter("CODE", "OTP_Code"),
+        };
+
+            var response = await smsIr.VerifySendAsync(user.PhoneNumber, templateId, verifySendParameters);
+
+            var sendResult = response.Data;
+            var messageId = sendResult.MessageId;
+            var cost = sendResult.Cost;
+        }
+    }
+}
+
